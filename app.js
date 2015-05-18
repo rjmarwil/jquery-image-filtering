87ace6f7cb02f52c58a11019103ccebdc84fb318
Jake Marwil <rjm02006@gmail.com>
2015-05-18 09:55:14 -0600

    user can toggle boackground between dark and light wood grain

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 	console.log("The document is ready!");
+
+	$(".dark").click(function() {
+		$('body').css("background", "#000 url(img/dark_wood.png) repeat");
+	});
+
+	$(".light").click(function() {
+		$('body').css("background", "#000 url(img/tileable_wood_texture.png) repeat");
+	});
 })
