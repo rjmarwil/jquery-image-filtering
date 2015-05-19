62f5bbd3478773032785f2c50f64893774b1f583
Jake Marwil <rjm02006@gmail.com>
2015-05-19 14:01:13 -0600

    change from .click to .on('click'

--- app.js
+++ app.js
@@ -29,15 +29,15 @@ $(document).ready(function() {
 	}
 
 	// Background buttons
-	$(".dark").click(function() {
+	$(".dark").on("click", function() {
 		$('body').css("background", "#000 url(img/dark_wood.png) repeat");
 	});
 
-	$(".light").click(function() {
+	$(".light").on("click", function() {
 		$('body').css("background", "#000 url(img/tileable_wood_texture.png) repeat");
 	});
 
-	$(".forest").click(function() {
+	$(".forest").on("click", function() {
 		$('body').css("background", "#000 url(img/forest.jpg) no-repeat");
 		$('body').css("background-size", "cover");
 	});
